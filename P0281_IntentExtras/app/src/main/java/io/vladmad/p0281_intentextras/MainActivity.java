c1ec12f0b929492b776c438fc8a858977ba6d5ab
Vlad Lx <32512346+Lxbinary@users.noreply.github.com>
2018-04-23 23:42:12 +0300

    P0291_SimpleActivityResult

--- P0281_IntentExtras/app/src/main/java/io/vladmad/p0281_intentextras/MainActivity.java
+++ P0281_IntentExtras/app/src/main/java/io/vladmad/p0281_intentextras/MainActivity.java
@@ -1,13 +1,31 @@
 package io.vladmad.p0281_intentextras;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText editText;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, ViewActivity.class);
+        intent.putExtra("name", editText.getText().toString());
+        startActivity(intent);
     }
 }
---
+++ P0281_IntentExtras/app/src/main/java/io/vladmad/p0281_intentextras/ViewActivity.java
@@ -0,0 +1,24 @@
+package io.vladmad.p0281_intentextras;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ViewActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+
+        textView = findViewById(R.id.textView);
+        Intent intent = getIntent();
+
+        String name = intent.getStringExtra("name");
+
+        textView.setText(name);
+    }
+}
